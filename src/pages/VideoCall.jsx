530f1e7f75c366ce7b577e2ace34b759932a3e8e
Vinayak Rastogi <vinayakr841@gmail.com>
2026-05-05 23:12:41 +0530

    hi

--- src/pages/VideoCall.jsx
+++ src/pages/VideoCall.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const VideoCall = ()=>{
     return (<>
-        <div className='font-bold'>Hello</div>
+        <div className='font-bold'>Hello, Bitch
+            
+        </div>
     </>)
 }
 
